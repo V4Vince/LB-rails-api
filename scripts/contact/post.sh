e83f11ceb8963089c98833cc670afee17f3049e0
V4Vince <vince.guoo@gmail.com>
2016-12-01 12:13:56 -0500

    Updates TOKEN in contacts-POST cURL

--- scripts/contact/post.sh
+++ scripts/contact/post.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-TOKEN=BAhJIiU1NDg5OTc5M2IxMGQ0NDY3OWZhNzExNWIzMzUwNjBhMAY6BkVG--5a5db79dcefc89aac19cfb3640a0e90022945003
+TOKEN=BAhJIiU1ZGFkYTdhOGJkYWE0ZjdjMTA5NWIyMjU2N2Q1ZDY4MQY6BkVG--922838e566340fd8b7b1716e92ba9018835a18e7
 
 curl --include --request POST http://localhost:4741/contacts \
   --header "Content-Type: application/json" \
@@ -10,5 +10,35 @@ curl --include --request POST http://localhost:4741/contacts \
     "contact": {
       "contact_type": "Email",
       "contact_path": "Fake@gmail.com"
+    }
+  }'
+
+curl --include --request POST http://localhost:4741/contacts \
+--header "Content-Type: application/json" \
+--header "Authorization: Token token=$TOKEN" \
+--data '{
+  "contact": {
+    "contact_type": "Phone",
+    "contact_path": "(978) 999-0000"
+  }
+}'
+
+curl --include --request POST http://localhost:4741/contacts \
+--header "Content-Type: application/json" \
+--header "Authorization: Token token=$TOKEN" \
+--data '{
+  "contact": {
+    "contact_type": "Facebook",
+    "contact_path": "facebook.com/myfacebook"
+  }
+}'
+
+curl --include --request POST http://localhost:4741/contacts \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "contact": {
+      "contact_type": "Twitter",
+      "contact_path": "twitter.com/me"
     }
   }'
